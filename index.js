104e02b5f9b199a499208d163319b082afb0cf55
denniskioko <kiokodennis6990@gmail.com>
2023-12-19 14:13:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply)
+
+
+let random = Math.floor(Math.random() *10) + 1;
+console.log(random)
+
+
+const num3 = 29;
+const num4 = 5;
+let mod = 29 % 5;
+console.log(mod)
+
+
+let max = Math.max (1, 5, 10, 12, 18,20)
+console.log(max)
